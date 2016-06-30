919acb0fe79273cea7aeb27fc9430745a4297539
liuwenzhi <liuwenzhi15@163.com>
2016-06-30 22:39:16 +0800

    create some code test

--- HelloGithub.java
+++ HelloGithub.java
@@ -1,5 +1,10 @@
 public class HelloGithub{
 	public static void main(String[] args){
 		System.out.println("hello github");
+		show();
+	}
+	
+	public static void show(){
+		System.out.println("test upload local file");
 	}
 }
