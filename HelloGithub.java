90a4aabcabf639e068777f3625c825f4a04fb0e3
liuwenzhi <liuwenzhi15@163.com>
2016-07-04 20:35:50 +0800

    提交

--- HelloGithub.java
+++ HelloGithub.java
@@ -2,7 +2,4 @@ public class HelloGithub{
 	public static void main(String[] args){
 		System.out.println("hello github"); 
 	}  
-	//分支合并到master
-	//git merge 分支名a  先切换到即将合并到的分支在执行此命令
-	//意思是将分支a合并到当前分支
 }
